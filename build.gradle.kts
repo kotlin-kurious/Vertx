b44c6140f0215e2cb5b3905ad7829e59f44e6f54
arisromil <arisromil@yahoo.com>
2023-08-01 15:01:34 -0600

    fix compile err

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,11 +14,15 @@ repositories {
 
 val vertxVersion = "4.1.5"
 dependencies {
-    testImplementation(kotlin("test"))
     implementation("io.vertx:vertx-core:$vertxVersion")
     implementation("io.vertx:vertx-web:$vertxVersion")
+    implementation("io.vertx:vertx-web-client:$vertxVersion")
     implementation("io.vertx:vertx-lang-kotlin:$vertxVersion")
     implementation("io.vertx:vertx-lang-kotlin-coroutines:$vertxVersion")
+    implementation("io.vertx:vertx-pg-client:$vertxVersion")
+    implementation("org.postgresql:postgresql:42.3.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.0")
 }
 
 tasks.test {
--- src/main/kotlin/server.kt
+++ src/main/kotlin/server.kt
@@ -1,3 +1,12 @@
+import io.vertx.core.Vertx
+import io.vertx.core.json.JsonObject
+import io.vertx.ext.web.Router
+import io.vertx.ext.web.handler.BodyHandler
+import io.vertx.kotlin.core.json.json
+import io.vertx.kotlin.core.json.obj
+import io.vertx.kotlin.coroutines.CoroutineVerticle
+import kotlinx.coroutines.launch
+
 fun main() {
     val vertx = Vertx.vertx()
     vertx.createHttpServer().requestHandler{ ctx ->
