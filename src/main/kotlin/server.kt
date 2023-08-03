425c4eb73318f120b058227324fa27b480820a57
arisromil <arisromil@yahoo.com>
2023-08-03 08:49:34 -0600

    add a /status endpoint and use JSON for response

--- src/main/kotlin/server.kt
+++ src/main/kotlin/server.kt
@@ -14,3 +14,25 @@ fun main() {
     }.listen(8081)
     println("open http://localhost:8081")
 }
+
+private fun router(): Router {
+    val vertx = Vertx.vertx()
+    val router = Router.router(vertx)
+
+    val json = json {
+        obj(
+            "status" to "OK"
+        )
+    }
+
+    router.get("/status").handler { ctx ->
+        ctx.response()
+            .setStatusCode(200)
+            .end(json.toString())
+    }
+    vertx.createHttpServer()
+        .requestHandler(router)
+        .listen(8081)
+
+    return router
+}
